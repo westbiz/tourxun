b4adccda12d5a3b03de1d73fd84464e8ee9e146a
westbiz <westbiz@163.com>
2018-06-01 00:07:12 +0800

    home update

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -96,12 +96,13 @@ class ProductController extends Controller {
 			$form->display('id', 'ID');
 			$form->text('name', '名称');
 			$form->text('category_id', '分类');
-			$form->text('day', '天数');
-			$form->text('night', '晚数');
-			$form->text('hotel', '酒店星级');
+			$form->number('day', '天数')->min(1)->max(90)->default(1);
+			$form->number('night', '晚数')->min(0);
+			$form->number('hotel', '酒店星级')->min(3)->max(5)->default(3);
 			$form->text('comment_id', '评论');
 			$form->text('price_id', '价格id');
-			$form->text('star', '评星')->attribute(['class' => 'rating', 'min' => 0, 'max' => 5, 'step' => 1, 'step' => 1, 'data-size' => 'sm', 'value=' => 2]);
+			// $form->text('star', '评星')->attribute(['class' => 'rating', 'min' => 0, 'max' => 5, 'step' => 1, 'step' => 1, 'data-size' => 'sm', 'value=' => 2]);
+			$form->slider('star', '评星')->options(['max' => 5, 'min' => 1, 'step' => 0.5, 'postfix' => '星级']);
 			$form->text('summary', '概述');
 			$form->editor('content', '正文');
 			$form->radio('active', '激活')->options(['0' => '否', '1' => '是']);;
