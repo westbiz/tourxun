e3a0904a2a7d5b7b5aff65165d390e240b6370f6
westbiz <westbiz@163.com>
2018-11-16 18:00:25 +0800

    attrvalues

--- app/Admin/Controllers/AttrvalueController.php
+++ app/Admin/Controllers/AttrvalueController.php
@@ -2,125 +2,121 @@
 
 namespace App\Admin\Controllers;
 
-use App\Models\Attrvalue;
 use App\Http\Controllers\Controller;
+use App\Models\Attrvalue;
 use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
-class AttrvalueController extends Controller
-{
-    use HasResourceActions;
+class AttrvalueController extends Controller {
+	use HasResourceActions;
 
-    /**
-     * Index interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function index(Content $content)
-    {
-        return $content
-            ->header('Index')
-            ->description('description')
-            ->body($this->grid());
-    }
+	/**
+	 * Index interface.
+	 *
+	 * @param Content $content
+	 * @return Content
+	 */
+	public function index(Content $content) {
+		return $content
+			->header('Index')
+			->description('description')
+			->body($this->grid());
+	}
 
-    /**
-     * Show interface.
-     *
-     * @param mixed $id
-     * @param Content $content
-     * @return Content
-     */
-    public function show($id, Content $content)
-    {
-        return $content
-            ->header('Detail')
-            ->description('description')
-            ->body($this->detail($id));
-    }
+	/**
+	 * Show interface.
+	 *
+	 * @param mixed $id
+	 * @param Content $content
+	 * @return Content
+	 */
+	public function show($id, Content $content) {
+		return $content
+			->header('Detail')
+			->description('description')
+			->body($this->detail($id));
+	}
 
-    /**
-     * Edit interface.
-     *
-     * @param mixed $id
-     * @param Content $content
-     * @return Content
-     */
-    public function edit($id, Content $content)
-    {
-        return $content
-            ->header('Edit')
-            ->description('description')
-            ->body($this->form()->edit($id));
-    }
+	/**
+	 * Edit interface.
+	 *
+	 * @param mixed $id
+	 * @param Content $content
+	 * @return Content
+	 */
+	public function edit($id, Content $content) {
+		return $content
+			->header('Edit')
+			->description('description')
+			->body($this->form()->edit($id));
+	}
 
-    /**
-     * Create interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function create(Content $content)
-    {
-        return $content
-            ->header('Create')
-            ->description('description')
-            ->body($this->form());
-    }
+	/**
+	 * Create interface.
+	 *
+	 * @param Content $content
+	 * @return Content
+	 */
+	public function create(Content $content) {
+		return $content
+			->header('Create')
+			->description('description')
+			->body($this->form());
+	}
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
-    protected function grid()
-    {
-        $grid = new Grid(new Attrvalue);
+	/**
+	 * Make a grid builder.
+	 *
+	 * @return Grid
+	 */
+	protected function grid() {
+		$grid = new Grid(new Attrvalue);
 
-        $grid->id('ID');
-        $grid->attrvalue();
-        $grid->catattr_id();
-        $grid->created_at('Created at');
-        $grid->updated_at('Updated at');
+		$grid->id('ID');
+		$grid->attrvalue('属性值')->editable();
+		$grid->catattr()->name('属性')->color('#f00');
+		$grid->status('状态')->using(['1' => '是', '0' => '否']);
+		// $grid->created_at('Created at');
+		// $grid->updated_at('Updated at');
 
-        return $grid;
-    }
+		return $grid;
+	}
 
-    /**
-     * Make a show builder.
-     *
-     * @param mixed $id
-     * @return Show
-     */
-    protected function detail($id)
-    {
-        $show = new Show(Attrvalue::findOrFail($id));
+	/**
+	 * Make a show builder.
+	 *
+	 * @param mixed $id
+	 * @return Show
+	 */
+	protected function detail($id) {
+		$show = new Show(Attrvalue::findOrFail($id));
 
-        $show->id('ID');
-        $show->created_at('Created at');
-        $show->updated_at('Updated at');
+		$show->id('ID');
+		$show->attrvalue();
+		$show->created_at('Created at');
+		$show->updated_at('Updated at');
 
-        return $show;
-    }
+		return $show;
+	}
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        $form = new Form(new Attrvalue);
+	/**
+	 * Make a form builder.
+	 *
+	 * @return Form
+	 */
+	protected function form() {
+		$form = new Form(new Attrvalue);
 
-        $form->display('ID');
-        $form->text('attrvalue');
-        $form->display('Created at');
-        $form->display('Updated at');
+		$form->display('ID');
+		$form->text('attrvalue');
+		$form->text('order');
+		$form->text('status');
+		// $form->display('Created at');
+		// $form->display('Updated at');
 
-        return $form;
-    }
+		return $form;
+	}
 }
