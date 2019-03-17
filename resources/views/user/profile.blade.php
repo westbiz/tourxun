7c211fcf6cb5afc19b6e75fc6bfc2da8b38889ec
westbiz <westiz@163.com>
2019-03-17 23:05:27 +0800

    profile&app

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -19,7 +19,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-3">
+            <div class="card border-info mb-3">
+              <div class="card-header">
+                Featured
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+              </ul>
+            </div>
+        </div>
+        <div class="col-md-9">
             <div class="card">
                 <div class="card-header">Profile</div>
 
